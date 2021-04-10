f20cd07ca660a185b36878bda6a06fa59d5f56ba
Rajkumar Dubey <r.rajkumar.dubey@accenture.com>
2021-04-11 01:54:58 +0530

    basic folder structure with children and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder name="Desktop">
+        <Folder name="Music">
+          <File name="all_star.mp4" />
+          <File name="Arijit-mashups.mp3" />
+        </Folder>
+        {/* these are children props.children */}
+        <File name="Dog.png" />
+        <File name="Cat.png" />
+      </Folder>
+      <Folder name="Applications" />
     </div>
   );
 }
+
+const Folder = (props) => {
+  // console.log(props);
+  const [isOpen, setIsOpen] = useState(true);
+  const { name, children } = props;
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <span onClick={handleClick}>{name}</span>
+      <div style={{ marginLeft: "20px" }}>{isOpen ? children : null}</div>
+    </div>
+  );
+};
+
+const File = (props) => {
+  return <div>{props.name}</div>;
+};
 
 export default App;
