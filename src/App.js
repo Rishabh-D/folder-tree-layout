3da52cf319408429728e45d1ce46b7edd21754e5
Rajkumar Dubey <r.rajkumar.dubey@accenture.com>
2021-04-11 02:22:59 +0530

    added icons

--- src/App.js
+++ src/App.js
@@ -19,22 +19,44 @@ function App() {
 
 const Folder = (props) => {
   // console.log(props);
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const { name, children } = props;
+  const direction = isOpen ? "down" : "right";
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+  console.log(direction);
   return (
     <div>
-      <span onClick={handleClick}>{name}</span>
+      <span onClick={handleClick}>
+        <i className="blue folder icon" />
+        <i className={`caret ${direction} icon`} />
+      </span>
+      {name}
       <div style={{ marginLeft: "20px" }}>{isOpen ? children : null}</div>
     </div>
   );
 };
 
 const File = (props) => {
-  return <div>{props.name}</div>;
+  const { name } = props;
+  const fileExtension = name.split(".")[1];
+  const fileIcons = {
+    mp4: "headphones",
+    mp3: "play circle",
+    jpeg: "file image",
+    png: "file image outline",
+  };
+
+  return (
+    <div>
+      <span>
+        <i className={`caret ${fileIcons[fileExtension]} icon`}></i>
+      </span>
+      {props.name}
+    </div>
+  );
 };
 
 export default App;
